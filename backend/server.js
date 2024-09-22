d0348a4d6884b667056d1cff4a9754436efe404b
Techie1996 <130053227+Techie1996@users.noreply.github.com>
2024-09-22 20:45:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://aeroedge.netlify.app', // Allow requests from this origin
+}));
 app.use(express.json());
 
 // Connect to MongoDB
